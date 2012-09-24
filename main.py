e69a30110e6fe76cc87e34a0ac82a01ae5a52340
STALTZ <staltz@benaia>
2012-09-25 01:24:41 +0300

    NEW Replace 'delete' task with 'done' button; NEW Show done tasks in the bottom, muted

--- main.py
+++ main.py
@@ -13,16 +13,26 @@ class Task(db.Model):
 	"""Something to do"""
 	text = db.StringProperty()
 	date = db.DateTimeProperty(auto_now_add=True)
+	datedone = db.DateProperty()
+	done = db.BooleanProperty(default=False)
+
+	def getDateDone(self):
+		return self.datedone.strftime('%d.%m.%Y')
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
 		tasks = db.GqlQuery("SELECT * "
 		            "FROM Task "
+		            "WHERE done = FALSE "
 		            "ORDER BY date DESC")
+		done = db.GqlQuery("SELECT * "
+		                   "FROM Task "
+		                   "WHERE done = TRUE "
+		                   "ORDER BY date DESC LIMIT 4")
 
 		template_values = {
-		   'age': 25,
 		   'tasks': tasks,
+		   'done': done,
 		}
 
 		template = jinja_environment.get_template('templates/index.html')
@@ -35,19 +45,20 @@ class Inserter(webapp.RequestHandler):
 		task.put()
 		self.redirect('/')
 
-class Deleter(webapp.RequestHandler):
+class DoneHandler(webapp.RequestHandler):
 	def post(self):
 		taskid = int(self.request.get('taskid'))
 		task = Task.get_by_id(taskid)
-#		logging.info("Delete: Taskid=%s"%taskid)
-#		logging.info("Delete: task=%s"%task)
-		task.delete()
+		task.done = True
+		task.datedone = datetime.date.today()
+		logging.info(task.getDateDone())
+		task.put()
 		self.redirect('/')
 
 app = webapp.WSGIApplication([
 	('/', MainPage),
 	('/insertTask', Inserter),
-	('/deleteTask', Deleter),
+	('/doneTask', DoneHandler),
 	],
 	debug=True)
 
